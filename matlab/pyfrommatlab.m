12197f3d6f152c52187c379bbc92d227b659622e
Haruto Tanaka <hamuchan214@gmail.com>
2024-10-09 13:58:15 +0900

    Update pyfrommatlab.m
    とりあえず動きはした

--- matlab/pyfrommatlab.m
+++ matlab/pyfrommatlab.m
@@ -1,1 +1,12 @@
-pyrunfile('csma.py')
+clearAllMemoizedCaches
+
+int8 n;
+n = 20;
+int8 seed;
+seed = 123;
+
+pyrun("import csma");
+pyrun("n = 20")
+pyrun("seed = 123")
+pyrun("users = csma.create_users(n, seed)");
+pyrun("csma.simulate_transmission(users, 120, 24,print_output=csma.print_mode[2])")
